d5dcc8424a18d4030b0e45192bd43755ae75d5ce
kkboom <1396956549@qq.com>
2019-04-28 01:18:57 +0800

    map done

--- news/js/map.js
+++ news/js/map.js
@@ -10,7 +10,6 @@
 
 var MapOfSchool = echarts.init(document.getElementById('mapOfSchool'));
 
-var name_title = "中国人民大学2017年各省市计划录取人数"
 var subname = '数据爬取自中国教育网'
 var nameColor = " rgb(55, 75, 113)"
 var name_fontFamily = '等线'
@@ -19,73 +18,40 @@ var name_fontSize = 18
 var mapName = 'china'
 
 var data = [
-    {name:"北京",value:177},
-    {name:"天津",value:42},
-    {name:"河北",value:102},
-    {name:"山西",value:81},
-    {name:"内蒙古",value:47},
-    {name:"辽宁",value:67},
-    {name:"吉林",value:82},
-    {name:"黑龙江",value:66},
-    {name:"上海",value:24},
-    {name:"江苏",value:92},
-    {name:"浙江",value:114},
-    {name:"安徽",value:109},
-    {name:"福建",value:116},
-    {name:"江西",value:91},
-    {name:"山东",value:119},
-    {name:"河南",value:137},
-    {name:"湖北",value:116},
-    {name:"湖南",value:114},
-    {name:"重庆",value:91},
-    {name:"四川",value:125},
-    {name:"贵州",value:62},
-    {name:"云南",value:83},
-    {name:"西藏",value:9},
-    {name:"陕西",value:80},
-    {name:"甘肃",value:56},
-    {name:"青海",value:10},
-    {name:"宁夏",value:18},
-    {name:"新疆",value:67},
-    {name:"广东",value:123},
-    {name:"广西",value:59},
-    {name:"海南",value:14},
+    {name:"北京",value:2},
+    {name:"天津",value:0},
+    {name:"河北",value:7},
+    {name:"山西",value:1},
+    {name:"内蒙古",value:3},
+    {name:"辽宁",value:1},
+    {name:"吉林",value:0},
+    {name:"黑龙江",value:2},
+    {name:"上海",value:4},
+    {name:"江苏",value:7},
+    {name:"浙江",value:0},
+    {name:"安徽",value:2},
+    {name:"福建",value:2},
+    {name:"江西",value:6},
+    {name:"山东",value:3},
+    {name:"河南",value:5},
+    {name:"湖北",value:7},
+    {name:"湖南",value:5},
+    {name:"重庆",value:5},
+    {name:"四川",value:12},
+    {name:"贵州",value:1},
+    {name:"云南",value:0},
+    {name:"西藏",value:0},
+    {name:"陕西",value:1},
+    {name:"甘肃",value:1},
+    {name:"青海",value:0},
+    {name:"宁夏",value:0},
+    {name:"新疆",value:0},
+    {name:"广东",value:9},
+    {name:"广西",value:0},
+    {name:"海南",value:3},
     ];
     
 var geoCoordMap = {};
-var toolTipData = [ 
-    {name:"北京",value:[{name:"文科",value:95},{name:"理科",value:82}]},
-    {name:"天津",value:[{name:"文科",value:22},{name:"理科",value:20}]},
-    {name:"河北",value:[{name:"文科",value:60},{name:"理科",value:42}]},
-    {name:"山西",value:[{name:"文科",value:40},{name:"理科",value:41}]},
-    {name:"内蒙古",value:[{name:"文科",value:23},{name:"理科",value:24}]},
-    {name:"辽宁",value:[{name:"文科",value:39},{name:"理科",value:28}]},
-    {name:"吉林",value:[{name:"文科",value:41},{name:"理科",value:41}]},
-    {name:"黑龙江",value:[{name:"文科",value:35},{name:"理科",value:31}]},
-    {name:"上海",value:[{name:"文科",value:12},{name:"理科",value:12}]},
-    {name:"江苏",value:[{name:"文科",value:47},{name:"理科",value:45}]},
-    {name:"浙江",value:[{name:"文科",value:57},{name:"理科",value:57}]},
-    {name:"安徽",value:[{name:"文科",value:57},{name:"理科",value:52}]},
-    {name:"福建",value:[{name:"文科",value:59},{name:"理科",value:57}]},
-    {name:"江西",value:[{name:"文科",value:49},{name:"理科",value:42}]},
-    {name:"山东",value:[{name:"文科",value:67},{name:"理科",value:52}]},
-    {name:"河南",value:[{name:"文科",value:69},{name:"理科",value:68}]},
-    {name:"湖北",value:[{name:"文科",value:60},{name:"理科",value:56}]},
-    {name:"湖南",value:[{name:"文科",value:62},{name:"理科",value:52}]},
-    {name:"重庆",value:[{name:"文科",value:47},{name:"理科",value:44}]},
-    {name:"四川",value:[{name:"文科",value:65},{name:"理科",value:60}]},
-    {name:"贵州",value:[{name:"文科",value:32},{name:"理科",value:30}]},
-    {name:"云南",value:[{name:"文科",value:42},{name:"理科",value:41}]},
-    {name:"西藏",value:[{name:"文科",value:5},{name:"理科",value:4}]},
-    {name:"陕西",value:[{name:"文科",value:38},{name:"理科",value:42}]},
-    {name:"甘肃",value:[{name:"文科",value:28},{name:"理科",value:28}]},
-    {name:"青海",value:[{name:"文科",value:5},{name:"理科",value:5}]},
-    {name:"宁夏",value:[{name:"文科",value:10},{name:"理科",value:8}]},
-    {name:"新疆",value:[{name:"文科",value:36},{name:"理科",value:31}]},
-    {name:"广东",value:[{name:"文科",value:63},{name:"理科",value:60}]},
-    {name:"广西",value:[{name:"文科",value:29},{name:"理科",value:30}]},
-    {name:"海南",value:[{name:"文科",value:8},{name:"理科",value:6}]},
-];
 
 /*获取地图数据*/
 MapOfSchool.showLoading();
@@ -99,17 +65,6 @@ mapFeatures.forEach(function(v) {
 
 });
 
-// console.log("============geoCoordMap===================")
-// console.log(geoCoordMap)
-// console.log("================data======================")
-// console.log(data)
-// console.log(toolTipData)
-
-var max = 480,
-    min = 9; // todo 
-var maxSize4Pin = 100,
-    minSize4Pin = 20;
-
 var convertData = function(data) {
     var res = [];
     for (var i = 0; i < data.length; i++) {
@@ -118,6 +73,7 @@ var convertData = function(data) {
             res.push({
                 name: data[i].name,
                 value: geoCoord.concat(data[i].value),
+                num: data[i].value[2]
             });
         }
     }
@@ -139,49 +95,17 @@ option = {
     },
     tooltip: {
         trigger: 'item',
-        formatter: function(params) {
-            if (typeof(params.value)[2] == "undefined") {
-                var toolTiphtml = ''
-                for(var i = 0;i<toolTipData.length;i++){
-                    if(params.name==toolTipData[i].name){
-                        toolTiphtml += toolTipData[i].name+':<br>'
-                        for(var j = 0;j<toolTipData[i].value.length;j++){
-                            toolTiphtml+=toolTipData[i].value[j].name+':'+toolTipData[i].value[j].value+"<br>"
-                        }
-                    }
-                }
-                // console.log(toolTiphtml)
-                // console.log(convertData(data))
-                return toolTiphtml;
-            } else {
-                var toolTiphtml = ''
-                for(var i = 0;i<toolTipData.length;i++){
-                    if(params.name==toolTipData[i].name){
-                        toolTiphtml += toolTipData[i].name+':<br>'
-                        for(var j = 0;j<toolTipData[i].value.length;j++){
-                            toolTiphtml+=toolTipData[i].value[j].name+':'+toolTipData[i].value[j].value+"<br>"
-                        }
-                    }
-                }
-                // console.log(toolTiphtml)
-                // console.log(convertData(data))
-                return toolTiphtml;
-            }
-        }
-    },
-    legend: {
-        orient: 'vertical',
-        y: 'top',
-        x: 'bottom',
-        data: ['credit_pm2.5'],
-        textStyle: {
-            color: '#fff'
+        formatter: 
+        function(params) {
+            if(params.seriesName == '点') return params.data.value[2];
+            else if(params.data == undefined) return 'none';
+            else return params.data.name+': '+params.data.value+' 所';
         }
     },
     visualMap: {
         show: true,
         min: 0,
-        max: 200,
+        max: 12,
         left: '10%',
         top: 'center',
         text: ['高', '低'], // 文本，默认为数值文本
@@ -244,13 +168,10 @@ option = {
             type: 'scatter',
             coordinateSystem: 'geo',
             data: convertData(data),
-            symbolSize: function(val) {
-                return val[2] / 10;
-            },
             label: {
                 normal: {
                     formatter: '{b}',
-                    position: 'right',
+                    position: 'bottom',
                     show: true
                 },
                 emphasis: {
@@ -299,17 +220,18 @@ option = {
             coordinateSystem: 'geo',
             symbol: 'pin', //气泡
             symbolSize: function(val) {
-                var a = (maxSize4Pin - minSize4Pin) / (max - min);
-                var b = minSize4Pin - a * min;
-                b = maxSize4Pin - a * max;
-                return a * val[2] + b;
+                if(val[2] == 0) return 0;
+                else return val[2]*2+20;
             },
             label: {
                 normal: {
+                    formatter: function(params) {
+                        return params.data.value[2];
+                    },
                     show: true,
                     textStyle: {
                         color: '#fff',
-                        fontSize: 9,
+                        fontSize: 10,
                     }
                 }
             },
@@ -319,7 +241,7 @@ option = {
                 }
             },
             zlevel: 6,
-            data: convertData(data),
+            data: convertData(data)
         }
     ]
 };
